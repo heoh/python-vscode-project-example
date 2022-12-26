a4c1497bf8e0fb6c9ce1dbdf86084be35db6d184
heoh <h0h6h2h5@gmail.com>
2022-12-26 19:46:49 +0000

    Add add_two() for importing other source example

--- project/core/my_func.py
+++ project/core/my_func.py
@@ -1,2 +1,9 @@
+from project.util import two
+
+
 def add_one(value: int) -> int:
     return value + 1
+
+
+def add_two(value: int) -> int:
+    return value + two()
--- project/test/core/test_my_func.py
+++ project/test/core/test_my_func.py
@@ -1,7 +1,13 @@
-from project.core.my_func import add_one
+from project.core.my_func import add_one, add_two
 
 
 def test_add_one():
     assert add_one(5) == 6
     assert add_one(0) == 1
     assert add_one(-3) == -2
+
+
+def test_add_two():
+    assert add_two(5) == 7
+    assert add_two(0) == 2
+    assert add_two(-3) == -1
